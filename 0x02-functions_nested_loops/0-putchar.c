ee1ed764a4d8dab1ab6c3acb5f4174bf5b13980e
kewablay <kojokewajunior123@gmail.com>
2022-09-13 18:47:41 -0700

    Added function 1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/** 
+ * main - prints the string _putchar
+ * Return: 0 on success.
+ */
+
+int main(void) 
+{
+	printf("\_putchar \n");
+	return(0);
+}
+
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Introduction to nexted loops and functions
