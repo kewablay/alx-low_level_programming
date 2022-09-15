607e96b59f74f75411491c3d755aee2761b3e99d
kewablay <kojokewajunior123@gmail.com>
2022-09-14 20:14:02 -0700

    made changes to 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,19 @@
 #include "main.h"
-/** 
- * main - prints the string _putchar
- * Return: 0 on success.
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
  */
 
-int main(void) 
+int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char *sh = "_putchar";
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
 	_putchar('\n');
+	return (0);
 
-	return(0);
 }
-
