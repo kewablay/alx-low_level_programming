41838a1d88a9916d205fe3806412848d3a6c0478
kewablay <kojokewajunior123@gmail.com>
2022-09-14 20:07:54 -0700

    added changes

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,12 +15,13 @@ void print_to_98(int n)
 	{
 		while (n <= 98)
 		{
-		printf("%d", n);
-		if (n != 98)
-		{
+			printf("%d", n);
+			if (n != 98)
+			{
 			printf(", ");
-		}
+			}
 		n++;
+		}
 	}
 	else if (n > 98)
 	{
