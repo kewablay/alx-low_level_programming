4c6024b3a77e686b593b9d06053d5c6bfbc9d034
kewablay <87356028+kewablay@users.noreply.github.com>
2022-10-01 19:49:18 +0000

    Added new dir 0x04- argc_argv

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+/**
+ * _putchar - this is the main header for putchar
+ * @c: character
+ *
+ * Return: Always o
+ */
+
+int _putchar(char c);
+
+#endif /*MAIN_H*/
